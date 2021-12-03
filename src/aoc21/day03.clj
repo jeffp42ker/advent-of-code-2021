453ada83e5b33406a4af2cec09daee72e5e089f1
jeffp42ker <jeffp42ker@gmail.com>
2021-12-03 17:42:03 -0500

    day 03 cleanup

--- src/aoc21/day03.clj
+++ src/aoc21/day03.clj
@@ -20,28 +20,46 @@
 
 (defn input->vs [inp] (->> (str/split inp #"\n") (mapv #(str/split % #""))))
 
-(defn transpose [inp]
-  (apply mapv vector inp))
+(defn slice [n inp]
+  (mapv (fn [v] (nth v n)) inp))
+
+(defn slice-freq [n inp]
+  (let [freq (->> inp
+                  (slice n)
+                  frequencies
+                  (sort-by val #(compare %2 %1)))
+        tie? (apply = (vals freq))]
+    {:most-common (if tie? "1" (-> freq first key))
+     :least-common (-> freq last key)}))
+
+(defn slice-rate [criteria n inp]
+  (criteria (slice-freq n inp)))
+
+(defn rate [criteria inp]
+  (let [xs (range (count (first inp)))]
+    (apply str (mapv #(slice-rate criteria % inp) xs))))
+
+(comment
+  
+  (slice 0 (input->vs sample))
+  (slice-freq 0 (input->vs sample))
+  (rate :most-common (input->vs sample))
+
+  )
 
 (def input (data/input 2021 3))
 
 ;; Part 1
 
 (defn part-1 [inp]
-  (let [gamma-rate-str (->> inp
-                            transpose
-                            (mapv frequencies)
-                            (mapv (fn [m] (if (> (m "1") (m "0")) "1" "0")))
-                            (str/join))
-        epsilon-rate-str (->> gamma-rate-str
-                             (mapv (fn [x] (if (= x \1) "0" "1")))
-                              str/join)]
-    (apply * (mapv #(Integer/parseInt % 2) [gamma-rate-str epsilon-rate-str]))))
+  (let [[gamma-rate epsilon-rate] (mapv #(rate % inp) [:most-common :least-common])
+        power-consumption (apply * (mapv #(Integer/parseInt % 2)
+                                         [gamma-rate epsilon-rate]))]
+    power-consumption))
 
 
 (comment
 
-(transpose (input->vs sample))  
 (part-1 (input->vs sample))
 (part-1 (input->vs input))
 
@@ -49,39 +67,32 @@
 
 ;; Part 2
 
-(defn bit-freq [inp n]
-  (let [slice (nth (transpose inp) n)
-        freq (->> (frequencies slice) (sort-by val #(compare %2 %1)))]
-    {:slice-pos n
-     :freq freq
-     :most-common (if (= (val (first freq))
-                         (val (second freq))) "1" (->> freq first key))
-     :least-common (->> freq last key)
-     :input inp}))
-
 (defn rating [bit-criteria inp]
-  (loop [slice-pos 0
-         input (filter #(= (bit-criteria (bit-freq inp slice-pos))
-                           (nth % slice-pos)) inp)]
+  (loop [pos 0
+         input (filter #(= (bit-criteria (slice-freq pos inp))
+                           (nth % pos)) inp)]
     (if (= 1 (count input))
       (Integer/parseInt (->> input first str/join) 2)
-      (recur (inc slice-pos)
-             (filter #(= (bit-criteria (bit-freq input (inc slice-pos)))
-                         (nth % (inc slice-pos))) input)))))
+      (recur (inc pos)
+             (filter #(= (bit-criteria (slice-freq (inc pos) input))
+                         (nth % (inc pos))) input)))))
 
 (defn part-2 [inp]
   (let [oxygen-generator-rating (rating :most-common inp)
-        co2-scrubber-rating (rating :least-common inp)]
-    (* oxygen-generator-rating co2-scrubber-rating)))
+        co2-scrubber-rating (rating :least-common inp)
+        life-support-rating
+        (* oxygen-generator-rating co2-scrubber-rating)]
+    life-support-rating))
 
 
 (comment
 
-  (bit-freq (input->vs sample) 1)
+  (slice-freq 1 (input->vs sample))
+
   (rating :most-common (input->vs sample))
   (rating :least-common (input->vs sample))
-  (part-2 (input->vs sample))
-  (part-2 (input->vs input))
 
+  (part-2 (input->vs sample))
+  (time (part-2 (input->vs input)))
 
   )
